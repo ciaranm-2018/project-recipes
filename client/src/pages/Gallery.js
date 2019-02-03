a77f07e64ee07e603f947e538bcf29734bbe6a94
Ciaran Mitchell <ciaranmitchell88@gmail.com>
2019-02-03 22:01:35 +0000

    recepe view fe page

--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -1,23 +1,16 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {withStyles} from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 import RecipeReviewCard from '../components/RecipeReviewCard';
 import FormDialog from '../components/FormDialog';
+import ViewRecipeDialog from '../components/ViewRecipeDialog';
 import Grid from '@material-ui/core/Grid';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
-import InputBase from '@material-ui/core/InputBase';
-import SearchIcon from '@material-ui/icons/Search';
 import Tooltip from '@material-ui/core/Tooltip';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import Loader from '../components/Loader';
 import NotificationBar from '../components/NotificationBar';
+import CustomizedInputBase from '../components/CustomizedInputBase';
 
 //
 import axios from 'axios';
@@ -36,15 +29,13 @@ const styles = {
 class Gallery extends Component {
   state = {
     addNewRecipeOpen: false,
+    viewRecipeOpen: false,
     galleryLoading: true,
-    recipes: [
-      {recipeName: 'name 1', description: 'description 1'},
-      {recipeName: 'name 2', description: 'description 2'},
-      {recipeName: 'name 3', description: 'description 3'},
-    ],
+    recipes: [],
     error: {},
     data: {},
-    notifcation : {messages:[],variant:'success', showMessages:false}
+    notification: {messages: [], variant: 'success', showMessages: false},
+    viewRecipeData: {},
   };
 
   handleChange = name => event => {
@@ -52,7 +43,6 @@ class Gallery extends Component {
   };
 
   validator = data => {
-    console.log ('validator', data);
     let isValidData = true;
     const error = {};
     for (let property in data) {
@@ -68,99 +58,334 @@ class Gallery extends Component {
   };
 
   hidenotification = () => {
-    this.setState({notifcation:{showMessages:false}})
-  }
+    this.setState ({notification: {showMessages: false}});
+  };
 
   handleSave = () => {
     if (this.validator (this.state.data)) {
-      this.setState ({galleryLoading: true, notifcation:{showMessages:false}});
+      this.setState ({
+        galleryLoading: true,
+        notification: {showMessages: false},
+      });
       axios
-        .post (
-          'http://localhost:5000/api/recepies',
-          this.state.data
-        )
+        .post ('https://project-recipes-ciaranm.c9users.io:8081/api/recepies', this.state.data)
         .then (response => {
-          console.log (response);
           this.setState ({
             addNewRecipeOpen: false,
             galleryLoading: false,
-            data: {},
-            notifcation : {messages:["Your recipe has been successfully submitted."],variant:'success', showMessages:true}
+            notification: {
+              messages: ['Your recipe has been successfully submitted.'],
+              variant: 'success',
+              showMessages: true,
+            },
           });
-          setTimeout(this.hidenotification, 5000)
-          
+          this.getRecipes ();
+          setTimeout (this.hidenotification, 5000);
         })
         .catch (error => {
           this.setState ({
             addNewRecipeOpen: false,
             galleryLoading: false,
-            notifcation : {messages:["something went wrong please try again"],variant:'error', showMessages:true}
+            notification: {
+              messages: ['something went wrong please try again'],
+              variant: 'error',
+              showMessages: true,
+            },
+          });
+          setTimeout (this.hidenotification, 5000);
+        });
+    } else {
+      console.log ('invalid');
+    }
+  };
+
+  handleUpdate = () => {
+    if (this.validator (this.state.data)) {
+      this.setState ({
+        galleryLoading: true,
+        notification: {showMessages: false},
+      });
+      axios
+        .post ('https://project-recipes-ciaranm.c9users.io:8081/api/recepe/', this.state.data)
+        .then (response => {
+          console.log (response);
+          this.setState ({
+            addNewRecipeOpen: false,
+            galleryLoading: false,
+            notification: {
+              messages: ['Your recipe has been successfully updated.'],
+              variant: 'success',
+              showMessages: true,
+            },
+          });
+          this.getRecipes ();
+          setTimeout (this.hidenotification, 5000);
+        })
+        .catch (error => {
+          this.setState ({
+            addNewRecipeOpen: false,
+            galleryLoading: false,
+            notification: {
+              messages: ['something went wrong please try again'],
+              variant: 'error',
+              showMessages: true,
+            },
           });
           console.log (error);
-          setTimeout(this.hidenotification, 5000)
-        })
+          setTimeout (this.hidenotification, 5000);
+        });
+    } else {
+      console.log ('invalid');
     }
   };
 
   handleClose = () => {
-    this.setState ({addNewRecipeOpen: false, data: {}, error: {}});
+    this.setState ({
+      addNewRecipeOpen: false,
+      data: {
+        calories: '',
+        cookTime: '',
+        description: '',
+        fat: '',
+        ingredients: '',
+        instructions: '',
+        prepTime: '',
+        recipeName: '',
+        totalTime: '',
+        yield: '',
+      },
+      error: {},
+    });
   };
 
-  handleOpen = () => {
+  handleViewRecipeClose = () => {
+    this.setState ({viewRecipeOpen: false, error: {}});
+  };
+
+  handleOpen = data => {
     this.setState ({
       addNewRecipeOpen: true,
       data: {
-        calories: null,
-        cookTime: null,
-        description: null,
-        fat: null,
-        ingredients: null,
-        instructions: null,
-        prepTime: null,
-        recipeName: null,
-        totalTime: null,
-        yield: null,
+        calories: '',
+        cookTime: '',
+        description: '',
+        fat: '',
+        ingredients: '',
+        instructions: '',
+        prepTime: '',
+        recipeName: '',
+        totalTime: '',
+        yield: '',
+        ...data,
       },
     });
   };
 
-  componentDidMount () {
+  handleViewRecipeOpen = id => {
+    // local update view count
+    const clonedRecipes = JSON.parse (JSON.stringify (this.state.recipes));
+    clonedRecipes.forEach (recipe => {
+      if (recipe.id === id) {
+        recipe.matrics.views += 1;
+      }
+    });
+
+    this.setState ({galleryLoading: true});
     axios
-      .get ('http://localhost:5000/api/recepies')
+      .get (`https://project-recipes-ciaranm.c9users.io:8081/api/recepe/${id}`)
       .then (response => {
         console.log (response.data);
+        this.setState ({
+          viewRecipeOpen: true,
+          galleryLoading: false,
+          viewRecipeData: response.data,
+          recipes: clonedRecipes,
+        });
+      })
+      .catch (error => {
+        this.setState ({
+          addNewRecipeOpen: false,
+          galleryLoading: false,
+          notification: {
+            messages: ['something went wrong please try again'],
+            variant: 'error',
+            showMessages: true,
+          },
+        });
+        console.log (error);
+        setTimeout (this.hidenotification, 5000);
+      });
+  };
+
+  handleDelete = id => {
+    this.setState ({
+      galleryLoading: true,
+      notification: {showMessages: false},
+    });
+    axios
+      .delete (`https://project-recipes-ciaranm.c9users.io:8081/api/recepe/${id}`)
+      .then (response => {
+        this.setState ({
+          galleryLoading: false,
+          notification: {
+            messages: ['Successfully deleted.'],
+            variant: 'success',
+            showMessages: true,
+          },
+          viewRecipeOpen: false,
+          error: {},
+          viewRecipeData: {},
+        });
+        this.getRecipes ();
+        setTimeout (this.hidenotification, 5000);
+      })
+      .catch (error => {
+        console.log (error);
+      });
+  };
+  handleChangeBasicSearch = data => {
+    this.setState ({
+      galleryLoading: true,
+      notification: {showMessages: false},
+    });
+
+    if (!data) {
+      data = '-';
+    }
+
+    axios
+      .get (`https://project-recipes-ciaranm.c9users.io:8081/api/recepe/search/${data}`)
+      .then (response => {
+        if (response.data.length === 0) {
+          this.setState ({
+            recipes: [],
+            galleryLoading: false,
+            notification: {
+              messages: ['No Matching Result'],
+              variant: 'info',
+              showMessages: true,
+            },
+          });
+        }
         this.setState ({recipes: response.data, galleryLoading: false});
       })
       .catch (error => {
         console.log (error);
       });
+  };
+
+  getRecipes = () => {
+    this.setState ({
+      galleryLoading: true,
+    });
+    axios
+      .get ('https://project-recipes-ciaranm.c9users.io:8081/api/recepies')
+      .then (response => {
+        console.log (response.data);
+        this.setState ({recipes: response.data, galleryLoading: false});
+      })
+      .catch (error => {
+        this.setState ({
+          galleryLoading: false,
+          notification: {
+            messages: ['something went wrong please try again'],
+            variant: 'error',
+            showMessages: true,
+          },
+        });
+        console.log (error);
+        setTimeout (this.hidenotification, 5000);
+      });
+  };
+
+  handleUpdateClick = data => {
+    this.setState ({viewRecipeOpen: false}, () => this.handleOpen (data));
+  };
+
+  handleRecipeVote = (data, id) => {
+    console.log (data, id);
+    // local update view count
+    const clonedRecipes = JSON.parse (JSON.stringify (this.state.recipes));
+    clonedRecipes.forEach (recipe => {
+      if (recipe.id === id) {
+        if (data == 'upVotes') {
+          recipe.matrics.upVotes += 1;
+        } else {
+          recipe.matrics.downVotes += 1;
+        }
+      }
+    });
+    this.setState ({recipes:clonedRecipes });
+
+    axios
+    .post ('https://project-recipes-ciaranm.c9users.io:8081/api/recepe/vote', {id,action:data})
+    .then (response => {
+      console.log (response.data);
+    })
+    .catch (error => {
+      console.log (error);
+    });
+  };
+
+  handleReset = () => {
+    this.setState ({notification: {showMessages: false}});
+    this.getRecipes ();
+  };
+
+  componentDidMount () {
+    this.getRecipes ();
   }
 
   render () {
     const {classes} = this.props;
     return (
       <div>
-        <NotificationBar variant={this.state.notifcation.variant} messages = {this.state.notifcation.messages} showMessages ={this.state.notifcation.showMessages} />
+
         <Loader open={this.state.galleryLoading} />
-        <Grid container direction="row" justify="flex-end" alignItems="center">
-          <Tooltip title="Add new recipe" aria-label="Add new recipe">
+        <Grid
+          style={{paddingBottom: 10}}
+          container
+          direction="row"
+          justify="flex-end"
+          alignItems="center"
+        >
+
+          <CustomizedInputBase
+            handleChangeBasicSearch={this.handleChangeBasicSearch}
+            handleReset={this.handleReset}
+          />
+          <Tooltip
+            style={{marginLeft: 20}}
+            title="Add new recipe"
+            aria-label="Add new recipe"
+          >
             <Fab
               color="primary"
               aria-label="Add"
               className={classes.fab}
-              onClick={this.handleOpen}
+              onClick={() => this.handleOpen ()}
             >
               <AddIcon />
             </Fab>
           </Tooltip>
+
         </Grid>
 
-        <Grid container spacing={24}>
+        <NotificationBar
+          variant={this.state.notification.variant}
+          messages={this.state.notification.messages}
+          showMessages={this.state.notification.showMessages}
+        />
+
+        <Grid container spacing={24} style={{marginTop: 10}}>
+
           {this.state.recipes.map ((recipe, index) => (
             <RecipeReviewCard
               key={index}
               recipeName={recipe.recipeName}
               summary={recipe.description}
+              {...recipe}
+              handleViewRecipeOpen={() => this.handleViewRecipeOpen (recipe.id)}
             />
           ))}
         </Grid>
@@ -169,8 +394,19 @@ class Gallery extends Component {
           open={this.state.addNewRecipeOpen}
           handleChange={this.handleChange}
           handleSave={this.handleSave}
+          handleUpdate={this.handleUpdate}
           handleClose={this.handleClose}
           error={this.state.error}
+          data={this.state.data}
+        />
+        <ViewRecipeDialog
+          open={this.state.viewRecipeOpen}
+          handleClose={this.handleViewRecipeClose}
+          data={this.state.viewRecipeData}
+          handleDelete={id => this.handleDelete (id)}
+          handleUpdateClick={() =>
+            this.handleUpdateClick (this.state.viewRecipeData)}
+          handleRecipeVote={(data, id) => this.handleRecipeVote (data, id)}
         />
 
       </div>
