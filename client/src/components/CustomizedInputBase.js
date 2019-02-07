41b1a235cc138b9d7d9096f642956c5811df3b8e
ciaranm <ciaranmitchell88@gmail.com>
2019-02-08 00:39:25 +0530

    view components

--- client/src/components/CustomizedInputBase.js
+++ client/src/components/CustomizedInputBase.js
@@ -6,17 +6,18 @@ import InputBase from '@material-ui/core/InputBase';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
-
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
-
+import Chip from '@material-ui/core/Chip';
 
 const styles = {
   root: {
     padding: '2px 4px',
     display: 'flex',
     alignItems: 'center',
-    width: 400,
-    marginLeft: '40%',
+    width: 500,
+    //marginLeft: '40%',
   },
   input: {
     marginLeft: 8,
@@ -30,28 +31,59 @@ const styles = {
     height: 28,
     margin: 4,
   },
+  chip: {
+    marginLeft: '5px',
+  },
 };
 
 class SearchBar extends React.Component {
   state = {
     isAdvanceSearch: false,
-    basicSearchValue: ""
+    basicSearchValue: '',
+    anchorEl: null,
+    anchorElType: null,
   };
+
+  handleClick = event => {
+    this.setState ({anchorEl: event.currentTarget});
+  };
+
+  handleClose = () => {
+    this.setState ({anchorEl: null, anchorElType: null});
+  };
+  handleClickType = event => {
+    this.setState ({anchorElType: event.currentTarget});
+  };
+
+  handleFilter = data => {
+    this.props.handleChangeBasicSearch(data)
+    this.handleClose ();
+  };
+
   render () {
-    const {classes, handleChangeBasicSearch, handleReset} = this.props;
+    const {anchorEl, anchorElType} = this.state;
+    const {
+      classes,
+      handleChangeBasicSearch,
+      handleReset,
+      countryMap,
+      mealTypeMap,
+    } = this.props;
+
+
 
     return (
       <Paper className={classes.root} elevation={1}>
         <InputBase
           className={classes.input}
           placeholder="Search Recipes"
-          onChange={e => this.setState({basicSearchValue: e.target.value})}
-          value = {this.state.basicSearchValue}
+          onChange={e => this.setState ({basicSearchValue: e.target.value})}
+          value={this.state.basicSearchValue}
         />
         <IconButton
           className={classes.iconButton}
           aria-label="Search"
-          onClick={() => handleChangeBasicSearch (this.state.basicSearchValue)}
+          onClick={() => handleChangeBasicSearch ({recipeName:this.state.basicSearchValue})}
         >
           <SearchIcon />
         </IconButton>
@@ -59,9 +91,9 @@ class SearchBar extends React.Component {
         <Button
           color="primary"
           className={classes.button}
-          onClick={() => {this.setState({basicSearchValue:''})
-          handleReset()
-          
+          onClick={() => {
+            this.setState ({basicSearchValue: ''});
+            handleReset ();
           }}
         >
           Reset
@@ -70,10 +102,69 @@ class SearchBar extends React.Component {
         <Button
           color="primary"
           className={classes.button}
-          onClick={() => console.log ('advance')}
+          aria-owns={anchorEl ? 'country-menu' : undefined}
+          aria-haspopup="true"
+          onClick={this.handleClick}
         >
-          Advanced
+          COUNTRY
         </Button>
+        <Divider className={classes.divider} />
+        <Button
+          color="primary"
+          className={classes.button}
+          aria-owns={anchorEl ? 'type-menu' : undefined}
+          aria-haspopup="true"
+          onClick={this.handleClickType}
+        >
+          TYPE
+        </Button>
+
+        <Menu
+          id="type-menu"
+          anchorEl={anchorElType}
+          open={Boolean (anchorElType)}
+          onClose={this.handleClose}
+        >
+          {mealTypeMap &&
+            mealTypeMap.map ((mealType, index) => (
+              <MenuItem
+                key={index}
+                onClick={() =>
+                  this.handleFilter ({mealType: mealType.mealType})}
+              >
+                {mealType.mealType}
+                {' '}
+                <Chip
+                  className={classes.chip}
+                  label={mealType.count}
+                  color="primary"
+                />
+              </MenuItem>
+            ))}
+
+        </Menu>
+        <Menu
+          id="country-menu"
+          anchorEl={anchorEl}
+          open={Boolean (anchorEl)}
+          onClose={this.handleClose}
+        >
+          {countryMap &&
+            countryMap.map ((country, index) => (
+              <MenuItem
+                key={index}
+                onClick={() => this.handleFilter ({country: country.country})}
+              >
+                {country.country}
+                {' '}
+                <Chip
+                  className={classes.chip}
+                  label={country.count}
+                  color="primary"
+                />
+              </MenuItem>
+            ))}
+        </Menu>
 
       </Paper>
     );
---
+++ client/src/components/constant.js
@@ -0,0 +1,99 @@
+export const mealType = [
+    {
+      value: 'breakfast',
+      label: 'breakfast',
+    },
+    {
+      value: 'brunch',
+      label: 'brunch',
+    },
+    {
+      value: 'lunch',
+      label: 'lunch',
+    },
+    {
+      value: 'dinner',
+      label: 'dinner',
+    },
+  ];
+  
+  export const countries = [
+    {
+      value: 'Austria',
+    },
+    {
+      value: 'Belgium',
+    },
+    {
+      value: 'Bulgaria',
+    },
+    {
+      value: 'Cyprus',
+    },
+    {
+      value: 'Czech Republic',
+    },
+    {
+      value: 'Germany',
+    },
+    {
+      value: 'Denmark',
+    },
+    {
+      value: 'Estonia',
+    },
+    {
+      value: 'Spain',
+    },
+    {
+      value: 'Finland',
+    },
+    {
+      value: 'United Kingdom',
+    },
+    {
+      value: 'Greece',
+    },
+    {
+      value: 'Hungary',
+    },
+    {
+      value: 'Croatia',
+    },
+    {
+      value: 'Italy',
+    },
+    {
+      value: 'Lithuania',
+    },
+    {
+      value: 'Luxembourg',
+    },
+    {
+      value: 'Latvia',
+    },
+    {
+      value: 'Malta',
+    },
+    {
+      value: 'Netherlands',
+    },
+    {
+      value: 'Poland',
+    },
+    {
+      value: 'Portugal',
+    },
+    {
+      value: 'Romania',
+    },
+    {
+      value: 'Sweden',
+    },
+    {
+      value: 'Slovenia',
+    },
+    {
+      value: 'Slovakia',
+    },
+  ];
